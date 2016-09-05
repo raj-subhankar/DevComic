5a4401b0d1357aa89fb71a5d37434bbd61acba0f
Subhankar Raj <subhankar.rj@gmail.com>
2016-09-05 21:19:18 +0530

    Create getComic()

--- app/src/main/java/com/yellowsoft/subhankar/devcomic/MainActivity.java
+++ app/src/main/java/com/yellowsoft/subhankar/devcomic/MainActivity.java
@@ -35,6 +35,35 @@ public class MainActivity extends AppCompatActivity {
         last = (Button) findViewById(R.id.btnLast);
         random = (Button) findViewById(R.id.btnRandom);
 
+
+        getComic(1);
+
     }
 
+    void getComic(int num) {
+
+        ApiInterface apiService =
+                ApiClient.getClient().create(ApiInterface.class);
+
+        String numStr = num + ".json";
+        Call<Comic> call = apiService.getComic(numStr);
+        call.enqueue(new Callback<Comic>() {
+            @Override
+            public void onResponse(Call<Comic> call, Response<Comic> response) {
+                Log.d("response", response.toString());
+                if (response != null) {
+                    Comic comic = response.body();
+                    Picasso.with(getApplicationContext()).load(comic.getCartoon_jpg()).into(comicImage);
+                    title.setText(comic.getTitle());
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<Comic> call, Throwable t) {
+                // Log error here since request failed
+                Log.e("TAG", t.toString());
+            }
+        });
+    }
 }
